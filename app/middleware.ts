a26adf5fab3eb9e7f90cf56bba23439ec96f411e
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-04-17 23:04:04 -0600

    middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,38 +1,37 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 
-//const [AUTH_USER, AUTH_PASS] = (process.env.HTTP_BASIC_AUTH || ':').split(':');
-const [AUTH_USER, AUTH_PASS] = ['cz', '4'];
-
-// Step 1. HTTP Basic Auth Middleware for Challenge
-export function middleware(req: NextRequest) {
-  if (!isAuthenticated(req)) {
-    return new NextResponse('Authentication required', {
-      status: 401,
-      headers: { 'WWW-Authenticate': 'Basic' },
-    });
-  }
-
-  return NextResponse.next();
-}
+// const [AUTH_USER, AUTH_PASS] = (process.env.HTTP_BASIC_AUTH || ':').split(':');
+const AUTH_USER = 'cz';
+const AUTH_PASS = '4';
 
 // Step 2. Check HTTP Basic Auth header if present
 function isAuthenticated(req: NextRequest) {
-  const authheader = req.headers.get('authorization') || req.headers.get('Authorization');
+  const authheader =
+    req.headers.get('authorization') || req.headers.get('Authorization');
 
   if (!authheader) {
     return false;
   }
 
-  const auth = Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+  const auth = Buffer.from(authheader.split(' ')[1], 'base64')
+    .toString()
+    .split(':');
   const user = auth[0];
   const pass = auth[1];
 
-  if (user == AUTH_USER && pass == AUTH_PASS) {
-    return true;
-  } else {
-    return false;
+  return user === AUTH_USER && pass === AUTH_PASS;
+}
+
+export function middleware(request: NextRequest) {
+  // Call our authentication function to check the request
+  if (!isAuthenticated(request)) {
+    // Respond with JSON indicating an error message
+    return Response.json(
+      { success: false, message: 'authentication failed' },
+      { status: 401 }
+    );
   }
+  return NextResponse.next();
 }
 
 // Step 3. Configure "Matching Paths" below to protect routes with HTTP Basic Auth
