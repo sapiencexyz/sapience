5676dd2184bf1d32ede42fcbf92add0b3029ca38
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-08-02 11:30:57 -0400

    improvements

--- packages/data/src/worker.ts
+++ packages/data/src/worker.ts
@@ -1,23 +1,31 @@
-import { Abi, createPublicClient, http } from 'viem';
+import { Abi, createPublicClient, http, webSocket } from 'viem';
 import { indexBaseFeePerGas } from './processes/chain';
 import { indexMarketEvents } from './processes/market';
-import { mainnet, hardhat } from 'viem/chains';
-import Foil from '@/protocol/deployments/13370/Foil.json';
+import { mainnet, sepolia, hardhat } from 'viem/chains';
+import FoilLocal from '@/protocol/deployments/13370/Foil.json';
+import FoilSepolia from '@/protocol/deployments/11155111/Foil.json';
 
 const mainnetPublicClient = createPublicClient({
   chain: mainnet,
-  transport: http(), // switch to websockets? should automatically switch poll default on watchContractEvent 
+  transport: webSocket()
+});
+
+const sepoliaPublicClient = createPublicClient({
+  chain: sepolia,
+  transport: webSocket()
 });
 
 hardhat.id = 13370 as any;
 export const cannonPublicClient = createPublicClient({
   chain: hardhat,
-  transport: http() // switch to websockets? should automatically switch poll default on watchContractEvent 
+  transport: http()
 });
 
 Promise.all([
-  indexBaseFeePerGas(mainnetPublicClient, `${hardhat.id}:${Foil.address}`),
-  indexMarketEvents(cannonPublicClient, Foil as {address: string, abi: Abi}) // unsure why this type assertion is necessary
+  indexBaseFeePerGas(mainnetPublicClient, `${hardhat.id}:${FoilLocal.address}`),
+  indexBaseFeePerGas(mainnetPublicClient, `${sepolia.id}:${FoilSepolia.address}`), // consolidate this with above?
+  indexMarketEvents(sepoliaPublicClient, FoilSepolia as {address: string, abi: Abi}),
+  indexMarketEvents(cannonPublicClient, FoilLocal as {address: string, abi: Abi})
 ]).catch(error => {
   console.error('Error running processes in parallel:', error);
 });
