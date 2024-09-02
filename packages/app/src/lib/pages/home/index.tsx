38fd5b06e35e082cc4da5cdab0eeb7ba54a9d56f
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-09-02 17:24:51 -0400

    redirect hp

--- packages/app/src/lib/pages/home/index.tsx
+++ packages/app/src/lib/pages/home/index.tsx
@@ -1,10 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+'use client';
+
+import FoilTestnet from '@/protocol/deployments/11155111/Foil.json';
 import { Flex, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/markets/11155111:${FoilTestnet.address}/1`);
+  }, [router]);
+
   return (
     <Flex direction="column" alignItems="center" gap={4} w="full">
       <Text size="lg" my="auto" opacity="0.33" fontStyle="italic">
-        Analytics dashboard coming soon
+        Redirecting...
       </Text>
     </Flex>
   );
