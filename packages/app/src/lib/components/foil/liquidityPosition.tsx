d5f7351e48705a117166adc4527f6be25b79f549
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-09-23 13:28:56 -0400

    remove collect fees ui

--- packages/app/src/lib/components/foil/liquidityPosition.tsx
+++ packages/app/src/lib/components/foil/liquidityPosition.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Button, Divider, Heading, Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useAccount } from 'wagmi';
 
@@ -25,25 +25,6 @@ const LiquidityPosition = () => {
         />
       </Box>
       <AddEditLiquidity nftId={nftId} refetchTokens={refetch} />
-      <Box hidden={nftId === 0}>
-        <Divider my={6} />
-        <Heading size="sm" mb={1}>
-          Collect Fees
-        </Heading>
-        <Text fontSize="sm" mb={2}>
-          Liquidity providers earn fees from traders when the market price is
-          within their low and high price.
-        </Text>
-        <Button size="sm" variant="brand" w="100%" mb={2}>
-          Redeem 0 wstETH
-        </Button>
-        <Text fontSize="sm" mb={0.5} display="none">
-          PnL: +0 wstETH (inclusive of fees)
-        </Text>
-        <Text fontSize="sm" display="none">
-          APY: 0%
-        </Text>
-      </Box>
     </Box>
   );
 };
