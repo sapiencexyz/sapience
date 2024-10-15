84fc5fd9de6eec94a75d5db98005a4989e21857f
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-10-15 11:18:25 -0600

    hide cannon transport on prod

--- packages/app/src/app/providers.tsx
+++ packages/app/src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import type { HttpTransport } from 'viem';
 import { defineChain } from 'viem';
 import { sepolia } from 'viem/chains';
 import { createConfig, http, WagmiProvider } from 'wagmi';
@@ -25,19 +26,24 @@ export const cannon = defineChain({
   rpcUrls: { default: { http: ['http://localhost:8545'] } },
 });
 
+const transports: Record<number, HttpTransport> = {
+  [sepolia.id]: http(
+    process.env.NEXT_PUBLIC_INFURA_API_KEY
+      ? `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
+      : 'https://ethereum-sepolia-rpc.publicnode.com'
+  ),
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  transports[cannon.id] = http('http://localhost:8545');
+}
+
 // Create the configuration
 const config = createConfig({
   ssr: true,
   chains: [cannon, sepolia],
   connectors: [injected()],
-  transports: {
-    [cannon.id]: http('http://localhost:8545'),
-    [sepolia.id]: http(
-      process.env.NEXT_PUBLIC_INFURA_API_KEY
-        ? `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
-        : 'https://ethereum-sepolia-rpc.publicnode.com'
-    ),
-  },
+  transports,
 });
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
