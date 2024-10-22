a74ece3d5ec320bc0eec3a20cf329f0237663f53
Leo M <20520621+leomassazza@users.noreply.github.com>
2024-10-22 14:00:22 -0300

    improve initialize flow

--- packages/protocol/src/vault/interfaces/IVault.sol
+++ packages/protocol/src/vault/interfaces/IVault.sol
@@ -4,4 +4,9 @@ pragma solidity >=0.8.25 <0.9.0;
 import "./IERC7540.sol";
 import "./IResolutionCallback.sol";
 
-interface IVault is IERC7540, IResolutionCallback {}
+interface IVault is IERC7540, IResolutionCallback {
+    function initializeFirstEpoch(
+        uint256 _initialStartTime,
+        uint160 _initialSqrtPriceX96
+    ) external;
+}
