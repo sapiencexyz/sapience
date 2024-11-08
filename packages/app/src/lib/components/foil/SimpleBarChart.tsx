5b0fb68d58717b6d9cdaaf63eb2afd2747652aab
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-11-08 10:38:33 -0500

    polish

--- packages/app/src/lib/components/foil/SimpleBarChart.tsx
+++ packages/app/src/lib/components/foil/SimpleBarChart.tsx
@@ -15,17 +15,21 @@ const SimpleBarChart = ({ data }: { data: any[] }) => {
   return (
     <div className="flex flex-1 relative w-full h-[100px]">
       {value.length ? (
-        <div className="min-h-[50px] w-fit absolute top-0 left-0 z-[2]">
-          <p>
-            <CountUp
-              delay={0}
-              start={prevValue}
-              end={parseInt(value, 10)}
-              duration={0.3}
-              separator=","
-            />
-            {' gas '}
-            <span className="text-sm text-muted-foreground ml-1">{label}</span>
+        <div className="min-h-[50px] w-full absolute top-0 left-0 z-[2]">
+          <p className="flex items-baseline">
+            <span>
+              <CountUp
+                delay={0}
+                start={prevValue}
+                end={parseInt(value, 10)}
+                duration={0.3}
+                separator=","
+              />
+              {' gas'}
+            </span>
+            <span className="text-xs text-muted-foreground ml-auto">
+              {label}
+            </span>
           </p>
         </div>
       ) : null}
@@ -33,17 +37,33 @@ const SimpleBarChart = ({ data }: { data: any[] }) => {
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
             data={data}
-            margin={{ top: 25, right: 0, bottom: 0, left: 0 }}
+            margin={{ top: 30, right: 0, bottom: 0, left: 0 }}
             onMouseMove={(state) => {
               if (state?.activePayload?.[0]) {
                 const newValue = state.activePayload[0].value.toString();
+
                 if (newValue !== value) {
                   setPrevValue(parseInt(value, 10) || 0);
                   setValue(newValue);
                   setLabel(
-                    new Date(
+                    `${new Date(
                       state.activePayload[0].payload.timestamp
-                    ).toLocaleString()
+                    ).toLocaleString(undefined, {
+                      year: '2-digit',
+                      month: 'numeric',
+                      day: 'numeric',
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    })} → ${new Date(
+                      data[(state.activeTooltipIndex || 0) + 1]?.timestamp ??
+                        state.activePayload[0].payload.timestamp
+                    ).toLocaleString(undefined, {
+                      year: '2-digit',
+                      month: 'numeric',
+                      day: 'numeric',
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    })}`
                   );
                 }
               }
