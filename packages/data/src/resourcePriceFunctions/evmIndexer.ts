466eaa1cc7effc6bfbbbda43fcd66f0f87a2cb24
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-11-25 16:45:59 -0500

    graceful fail if no block

--- packages/data/src/resourcePriceFunctions/evmIndexer.ts
+++ packages/data/src/resourcePriceFunctions/evmIndexer.ts
@@ -12,8 +12,8 @@ class EvmIndexer {
   }
 
   private async storeBlockPrice(block: Block, market: Market) {
-    const value = block.baseFeePerGas; // in wei
-    const used = block.gasUsed;
+    const value = block?.baseFeePerGas; // in wei
+    const used = block?.gasUsed;
     if (!value || !block.number) {
       console.error(
         `No baseFeePerGas for block ${block.number} on market ${market.chainId}:${market.address}`
