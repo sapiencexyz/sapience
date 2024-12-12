702daf74bcfd54255acf84a8f86be6447cfdebc9
Leo M <20520621+leomassazza@users.noreply.github.com>
2024-12-12 16:06:10 -0300

    fix test

--- packages/protocol/test/TradeModule/TradesDumb.t.sol
+++ packages/protocol/test/TradeModule/TradesDumb.t.sol
@@ -61,7 +61,7 @@ contract TradePositionDumb is TestTrade {
     uint256 PLUS_FEE_MULTIPLIER_D18 = 1.01 ether;
     uint256 MINUS_FEE_MULTIPLIER_D18 = 0.99 ether;
     uint256 constant MIN_TRADE_SIZE = 10_000; // 10,000 vGas
-    uint256 constant MIN_COLLATERAL = 0; // 0 wstETH; (don't use the minCollateral)
+    uint256 constant MIN_COLLATERAL = 10_000; // 10,000 wstETH;
 
     function setUp() public {
         collateralAsset = IMintableToken(
@@ -836,46 +836,6 @@ contract TradePositionDumb is TestTrade {
             expectedStateData,
             "Short2Long Reduce"
         );
-    }
-
-    function test_revertIf_TradePriceOutOfBounds() public {
-        int256 initialPositionSize = 1 ether;
-
-        uint256 positionId;
-
-        vm.startPrank(trader1);
-        positionId = addTraderPosition(foil, epochId, initialPositionSize);
-
-        // Send more collateral than required, just checking the position can be created/modified
-        vm.expectPartialRevert(Errors.TradePriceOutOfBounds.selector);
-        foil.modifyTraderPosition(
-            positionId,
-            initialPositionSize - 1,
-            0,
-            block.timestamp + 30 minutes
-        );
-
-        vm.stopPrank();
-    }
-
-    function test_revertIf_TradeWithSmallCollateralAmount() public {
-        int256 initialPositionSize = 1 ether;
-
-        uint256 positionId;
-
-        vm.startPrank(trader1);
-        positionId = addTraderPosition(foil, epochId, initialPositionSize);
-
-        // Send more collateral than required, just checking the position can be created/modified
-        vm.expectPartialRevert(Errors.TradePriceOutOfBounds.selector);
-        foil.modifyTraderPosition(
-            positionId,
-            initialPositionSize - 1,
-            0,
-            block.timestamp + 30 minutes
-        );
-
-        vm.stopPrank();
     }
 
     // //////////////// //
