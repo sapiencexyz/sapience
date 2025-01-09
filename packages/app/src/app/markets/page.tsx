54fbf60dd1274bd67716805b68202a648f5d8589
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-01-09 11:59:45 -0500

    markets page

--- packages/app/src/app/markets/page.tsx
+++ packages/app/src/app/markets/page.tsx
@@ -2,10 +2,7 @@ import MarketsTable from '~/lib/components/foil/marketsTable';
 
 const Market = () => {
   return (
-    <div className="container mx-auto p-8 max-w-7xl">
-      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight mb-4">
-        Foil Markets
-      </h1>
+    <div className="container mx-auto p-8 max-w-8xl">
       <MarketsTable />
     </div>
   );
