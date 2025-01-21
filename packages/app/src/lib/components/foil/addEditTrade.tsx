ae5581f54ae1b844443d443958e39625fd823614
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-01-21 13:38:53 -0500

    short fix attempt

--- packages/app/src/lib/components/foil/addEditTrade.tsx
+++ packages/app/src/lib/components/foil/addEditTrade.tsx
@@ -159,7 +159,7 @@ export default function AddEditTrade() {
 
   const desiredSizeInContractUnit = useMemo(() => {
     if (!isEdit) {
-      return isLong ? sizeChangeInContractUnit : -sizeChangeInContractUnit;
+      return sizeChangeInContractUnit;
     }
 
     const originalPositionIsLong = positionData?.vGasAmount > BigInt(0);
@@ -167,11 +167,8 @@ export default function AddEditTrade() {
       ? positionData?.vGasAmount || BigInt(0)
       : -(positionData?.borrowedVGas || BigInt(0));
 
-    return (
-      currentSize +
-      (isLong ? sizeChangeInContractUnit : -sizeChangeInContractUnit)
-    );
-  }, [isEdit, positionData, sizeChangeInContractUnit, isLong]);
+    return currentSize + sizeChangeInContractUnit;
+  }, [isEdit, positionData, sizeChangeInContractUnit]);
 
   // Collateral balance for current address/account
   const { data: collateralBalance } = useReadContract({
