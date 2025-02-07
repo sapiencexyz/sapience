a2f651879bb131b945c0c06d0a759342c5911b03
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-02-07 12:18:07 -0500

    lp form ux improvement

--- packages/app/src/components/Liquidity/LiquidityPriceInput/index.tsx
+++ packages/app/src/components/Liquidity/LiquidityPriceInput/index.tsx
@@ -4,7 +4,6 @@ import { useContext } from 'react';
 import type { Control, Path, FieldValues } from 'react-hook-form';
 import { Controller } from 'react-hook-form';
 
-import { useFoil } from '../../../lib/context/FoilProvider';
 import { FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { PeriodContext } from '~/lib/context/PeriodProvider';
@@ -18,22 +17,6 @@ interface Props<T extends FieldValues> {
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-const getTickSpacingForFee = (fee: number): number => {
-  if (fee === 100) {
-    return 1;
-  }
-  if (fee === 500) {
-    return 10;
-  }
-  if (fee === 3000) {
-    return 60;
-  }
-  if (fee === 10000) {
-    return 200;
-  }
-  return 0;
-};
-
 const LiquidityPriceInput = <T extends FieldValues>({
   label,
   name,
@@ -41,22 +24,11 @@ const LiquidityPriceInput = <T extends FieldValues>({
   isDisabled = false,
   onBlur: externalOnBlur,
 }: Props<T>) => {
-  const { stEthPerToken } = useFoil();
-  const { collateralAssetTicker, useMarketUnits, marketParams } =
-    useContext(PeriodContext);
+  const { collateralAssetTicker, useMarketUnits } = useContext(PeriodContext);
 
   const getCurrentUnit = () => {
     return useMarketUnits ? `Ggas/${collateralAssetTicker}` : 'gwei';
   };
-
-  // TODO: I don't think this is right
-  const tickSpacing = getTickSpacingForFee(marketParams.feeRate);
-  // Calculate the price ratio for one tick spacing
-  const priceRatio = 1.0001 ** tickSpacing;
-  // The step should be the minimum price change
-  const step = useMarketUnits
-    ? priceRatio - 1 // For market units, use the raw price ratio change
-    : ((priceRatio - 1) * (stEthPerToken || 0)) / 1e9; // For gwei, convert the price ratio change
 
   return (
     <div className="mb-4">
@@ -86,8 +58,7 @@ const LiquidityPriceInput = <T extends FieldValues>({
                 inputMode="decimal"
                 disabled={isDisabled}
                 onWheel={(e) => e.currentTarget.blur()}
-                className="pr-[120px]"
-                step={step}
+                className="pr-[120px] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
               />
               <div className="absolute inset-y-0 right-0 flex items-center px-3 border border-input bg-muted rounded-r-md">
                 {getCurrentUnit()}
