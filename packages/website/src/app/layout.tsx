577c6cecc5910d9132a0e202f5d85aa18e0693eb
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-02-07 15:02:41 -0500

    checkpoint

--- packages/website/src/app/layout.tsx
+++ packages/website/src/app/layout.tsx
@@ -9,14 +9,12 @@ import '@/lib/styles/globals.css';
 
 const APP_NAME = 'Foil';
 const APP_DESCRIPTION =
-  'Foil is a fully decentralized marketplace connecting producers of onchain computing resources with consumers.';
-const DEFAULT_OG_IMAGE = 'https://foil.xyz/assets/og-image.png';
+  'The fully decentralized marketplace for onchain resources';
 
 export const metadata: Metadata = {
-  title: { default: APP_NAME, template: '%s | Foil' },
+  title: APP_NAME,
   description: APP_DESCRIPTION,
   applicationName: APP_NAME,
-  metadataBase: new URL('https://foil.xyz'),
   appleWebApp: {
     capable: true,
     title: APP_NAME,
@@ -26,32 +24,17 @@ export const metadata: Metadata = {
     telephone: false,
   },
   openGraph: {
-    type: 'website',
     url: 'https://foil.xyz',
-    title: {
-      default: APP_NAME,
-      template: '%s | Foil'
-    },
+    title: APP_NAME,
     description: APP_DESCRIPTION,
-    siteName: APP_NAME,
-    locale: 'en_US',
-    images: [{
-      url: DEFAULT_OG_IMAGE,
-      width: 1200,
-      height: 630,
-      alt: 'Foil is a fully decentralized marketplace connecting producers of onchain computing resources with consumers.',
-    }],
+    images: {
+      url: 'https://foil.xyz/assets/og-image.png',
+      alt: 'foil.xyz og-image',
+    },
   },
   twitter: {
     creator: '@foilxyz',
-    site: '@foilxyz',
     card: 'summary_large_image',
-    title: {
-      default: APP_NAME,
-      template: '%s | Foil'
-    },
-    description: APP_DESCRIPTION,
-    images: [DEFAULT_OG_IMAGE],
   },
   icons: {
     icon: '/assets/favicon.svg',
@@ -60,17 +43,6 @@ export const metadata: Metadata = {
     other: {
       rel: 'mask-icon',
       url: '/assets/favicon.svg',
-    },
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
     },
   },
 };
