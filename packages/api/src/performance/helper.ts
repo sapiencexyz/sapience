13e01516e7816678346275ac4fff84d11b4a6094
The Llama <195907590+llamalm@users.noreply.github.com>
2025-03-23 10:32:24 -0500

    Leaderboard on GraphQL
    * checkpoint
    
    * compiles
    
    * looks like it works
    
    * fix
    
    * fix

--- packages/api/src/performance/helper.ts
+++ packages/api/src/performance/helper.ts
@@ -148,3 +148,24 @@ export function maxBigInt(a: bigint, b: bigint) {
 export function minBigInt(a: bigint, b: bigint) {
   return a < b ? a : b;
 }
+
+export function startOfCurrentInterval(
+  timestamp: number,
+  interval: number
+): number {
+  return Math.floor(timestamp / interval) * interval;
+}
+
+export function startOfNextInterval(
+  timestamp: number,
+  interval: number
+): number {
+  return (Math.floor(timestamp / interval) + 1) * interval;
+}
+
+export function getTimeWindow(from: number, to: number, interval: number) {
+  return {
+    from: startOfCurrentInterval(from, interval),
+    to: startOfNextInterval(to, interval),
+  };
+}
