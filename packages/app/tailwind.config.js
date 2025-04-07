8f5181a3b76afafdb5e35f879433b08e512d6e7a
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-04-07 10:34:11 -0400

    fixes

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "../../packages/ui/components/**/*.{js,ts,jsx,tsx}"],
     presets: [require('@foil/ui/tailwind-preset')],
     theme: {
         extend: {
