37f1be30b73a6a8d52b846aebfb3eeda3985911b
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-04-21 14:15:23 -0400

    update fixtures

--- packages/api/src/migrations/1745101065970-migration.ts
+++ packages/api/src/migrations/1745101065970-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1745101065970 implements MigrationInterface {
-    name = 'Migration1745101065970'
+  name = 'Migration1745101065970';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "market_group" ADD "collateralSymbol" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "market_group" ADD "collateralSymbol" character varying`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "market_group" DROP COLUMN "collateralSymbol"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "market_group" DROP COLUMN "collateralSymbol"`
+    );
+  }
 }
