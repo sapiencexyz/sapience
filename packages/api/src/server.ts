02db7124f51aeee41b5d3300bc96253d696af72e
Vladyslav Nekriach <37906830+ukitta555@users.noreply.github.com>
2025-04-24 17:48:08 -0400

    fix leaderboard trade count logic (#altszn) (#461)
    * fix leaderboard trade count logic (#altszn)
    
    * fix lint

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -51,6 +51,10 @@ const startServer = async () => {
 
   console.log('ResourcePerformanceManager - Starting');
   const resources = await resourceRepository.find();
+
+  // TESTING ONLY - SELECT A SINGLE RESOURCE SO THAT YOU WON'T HAVE TO CACHE EVERYTHING
+  // const resources = (await resourceRepository.find()).filter((res) => res.id === 8);
+
   const resourcePerformanceManager = ResourcePerformanceManager.getInstance();
   await resourcePerformanceManager.initialize(resources);
   console.log('ResourcePerformanceManager - Initialized');
