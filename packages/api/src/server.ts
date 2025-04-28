d0ca8dde5511e68919781b8cfae833483ce90551
ukitta555 <ukitta555@gmail.com>
2025-04-28 14:35:00 -0400

    hotfix for caching layer

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -24,8 +24,17 @@ initSentry();
 const startServer = async () => {
   await initializeDataSource();
 
-  // Initialize fixtures from fixtures.json
-  await initializeFixtures();
+  if (
+    process.env.NODE_ENV === 'development' &&
+    process.env.DATABASE_URL?.includes('render')
+  ) {
+    console.log(
+      'Skipping fixtures initialization since we are in development mode and using production database'
+    );
+  } else {
+    // Initialize fixtures from fixtures.json
+    await initializeFixtures();
+  }
 
   const apolloServer = await initializeApolloServer();
 
@@ -50,7 +59,19 @@ const startServer = async () => {
   }
 
   console.log('ResourcePerformanceManager - Starting');
-  const resources = await resourceRepository.find();
+
+  let resources;
+  if (
+    process.env.NODE_ENV === 'development' &&
+    process.env.DATABASE_URL?.includes('render')
+  ) {
+    console.log(
+      "WARNING: Initializing resources selectively so that we don't have to cache everything"
+    );
+    resources = (await resourceRepository.find()).filter((res) => res.id === 8);
+  } else {
+    resources = await resourceRepository.find();
+  }
 
   // TESTING ONLY - SELECT A SINGLE RESOURCE SO THAT YOU WON'T HAVE TO CACHE EVERYTHING
   // const resources = (await resourceRepository.find()).filter((res) => res.id === 8);
