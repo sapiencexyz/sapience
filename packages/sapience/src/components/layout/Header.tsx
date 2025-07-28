f4cf58d275548ee9cf455c676adc65f250ac1204
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2025-07-28 14:18:57 -0400

    ready

--- packages/sapience/src/components/layout/Header.tsx
+++ packages/sapience/src/components/layout/Header.tsx
@@ -216,24 +216,8 @@ const Header = () => {
           <NavLinks />
         </SidebarContent>
         <SidebarFooter>
-          <div className="flex flex-col gap-2 text-xs w-full ml-2 rounded-lg">
+          <div className="flex flex-col gap-2 text-xs w-full ml-4 rounded-lg">
             <div className="flex flex-col items-start gap-2 mb-2">
-              <span>Built on</span>
-              <a
-                href="https://convergeonchain.xyz"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/converge.svg"
-                  alt="Converge"
-                  width={80}
-                  height={24}
-                  className="dark:invert opacity-80 hover:opacity-100 transition-opacity duration-200"
-                />
-              </a>
-            </div>
-            <div className="flex flex-col items-start gap-2 mb-0.5">
               <span>Powered by</span>
               <a
                 href="https://ethena.fi"
@@ -249,8 +233,24 @@ const Header = () => {
                 />
               </a>
             </div>
+            <div className="flex flex-col items-start gap-2 mb-0.5">
+              <span>Built on</span>
+              <a
+                href="https://convergeonchain.xyz"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/converge.svg"
+                  alt="Converge"
+                  width={80}
+                  height={24}
+                  className="dark:invert opacity-80 hover:opacity-100 transition-opacity duration-200"
+                />
+              </a>
+            </div>
           </div>
-          <div className="flex items-center gap-2 p-2">
+          <div className="flex items-center gap-2 p-2 pl-4 pb-4 pt-4">
             <Button size="icon" className="h-6 w-6 rounded-full" asChild>
               <a
                 href="https://github.com/sapiencexyz/sapience"
