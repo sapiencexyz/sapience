767a3a6edb6fe7282dd837e039104d4a2ea4783e
nostradamus-agent <nostradamus-agent@users.noreply.github.com>
2026-02-23 19:28:06 +0000

    fix: restore IMintSponsor import in PredictionMarketEscrow

--- packages/protocol/src/v2/PredictionMarketEscrow.sol
+++ packages/protocol/src/v2/PredictionMarketEscrow.sol
@@ -10,6 +10,7 @@ import "./interfaces/IConditionResolver.sol";
 import "./interfaces/IPredictionMarketToken.sol";
 import "./interfaces/IV2Types.sol";
 import "./interfaces/IV2Events.sol";
+import "./interfaces/IMintSponsor.sol";
 import "./interfaces/IPredictionMarketTokenFactory.sol";
 import "./utils/SignatureValidator.sol";
 
