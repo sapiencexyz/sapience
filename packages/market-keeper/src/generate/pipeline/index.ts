f8f990ebd5ebdaa9caa7933672eec5e0fc576568
Vladyslav Nekriach <37906830+ukitta555@users.noreply.github.com>
2026-03-25 18:17:45 -0400

    Revert "chore(market-keeper): remove volume threshold filter (#1368)"
    This reverts commit 850c6b548054211047d330acfc0eec78ec076e1b.

--- packages/market-keeper/src/generate/pipeline/index.ts
+++ packages/market-keeper/src/generate/pipeline/index.ts
@@ -18,11 +18,15 @@ import type {
 import type { Filter, FilterStats, PipelineResult } from './types';
 
 // Import combinators
-import { UnionFilter } from './combinators';
+import { UnionFilter, IntersectionFilter } from './combinators';
 
 // Import all filters
 import { BinaryMarketsFilter } from './filters/binary-markets';
-import { type MarketGroup } from './filters/volume-threshold';
+import {
+  VolumeThresholdFilter,
+  MarketVolumeThresholdFilter,
+  type MarketGroup,
+} from './filters/volume-threshold';
 import {
   LiquidityThresholdFilter,
   MarketLiquidityThresholdFilter,
@@ -76,7 +80,10 @@ export const MARKET_FILTERS: Filter<PolymarketMarket>[] = [
  */
 export const GROUP_FILTERS: Filter<MarketGroup>[] = [
   new UnionFilter([
-    new LiquidityThresholdFilter(),
+    new IntersectionFilter([
+      new VolumeThresholdFilter(),
+      new LiquidityThresholdFilter(),
+    ]),
     new AlwaysIncludeGroupFilter(),
   ]),
 ];
@@ -90,7 +97,10 @@ export const GROUP_FILTERS: Filter<MarketGroup>[] = [
  */
 export const UNGROUPED_MARKET_FILTERS: Filter<PolymarketMarket>[] = [
   new UnionFilter([
-    new MarketLiquidityThresholdFilter(),
+    new IntersectionFilter([
+      new MarketVolumeThresholdFilter(),
+      new MarketLiquidityThresholdFilter(),
+    ]),
     new AlwaysIncludeMarketFilter(),
   ]),
 ];
